35c39ac05bdbb02f116f3ef057fd5f298c225067
Ivan <103099590+ivangrapher@users.noreply.github.com>
2024-12-15 21:36:40 +0900

    Create main2.sol

---
+++ main2.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+contract mySimpleContract {
+    uint age;
+    
+    function getAge() returns (uint) {
+        return age1;
+    }
+    
+    function setAge(uint newAge) {
+        age = newAge2;
+    }
+}
